cd57c8695cdc4a018464e0f02cbb1214ccb11741
ark150 <luda150@hotmail.com>
2018-12-03 19:05:54 -0600

    modificacion de las tabs, se agrega el metodo para poder moverte desde el listado, se modifica la pantalla verificar para utilizar como listado de mi miembros de equipo imcompleto

--- app/src/main/java/com/lania/mca18/adapter/ItemsListAdapter.java
+++ app/src/main/java/com/lania/mca18/adapter/ItemsListAdapter.java
@@ -1,5 +1,8 @@
 package com.lania.mca18.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,11 +17,16 @@ import com.lania.mca18.model.Equipo;
 import com.lania.mca18.model.Item;
 import com.lania.mca18.model.Persona;
 import com.lania.mca18.registrosdci.R;
+import com.lania.mca18.registrosdci.RegistroESFragment;
+import com.lania.mca18.registrosdci.VerificacionActivity;
 
 import java.util.List;
 
 public class ItemsListAdapter extends RecyclerView.Adapter<ItemsListAdapter.ItemViewHolder> {
     private List<Item> items;
+    private int band;
+    Context context;
+
 
     public static class ItemViewHolder extends RecyclerView.ViewHolder
     {
@@ -60,9 +68,11 @@ public class ItemsListAdapter extends RecyclerView.Adapter<ItemsListAdapter.Item
             viewHolder.txvTitle.setText(((Persona) this.items.get(i)).getNombre());
             viewHolder.txvSecondary.setText(((Persona)
                     this.items.get(i)).getEquipo().getNombre());
+            band=0;
         } else if(this.items.get(i) instanceof Equipo)
         {
             viewHolder.txvTitle.setText(((Equipo) this.items.get(i)).getNombre());
+            band=1;
         }
         viewHolder.rvItems.setTag(this.items.get(i).getId());
         viewHolder.rvItems.setOnClickListener(new View.OnClickListener()
@@ -70,11 +80,24 @@ public class ItemsListAdapter extends RecyclerView.Adapter<ItemsListAdapter.Item
             @Override
             public void onClick(View view)
             {
-                Toast.makeText(view.getContext(),
-                        "ID: " + String.valueOf(view.getTag()),
-                        Toast.LENGTH_SHORT).show();
+
 
                 // Evento OnClick
+                if(band==0){
+                    Toast.makeText(view.getContext(),
+                            "ID: " + String.valueOf(view.getTag()),
+                            Toast.LENGTH_SHORT).show();
+                    Intent i= new Intent(view.getContext(), RegistroESFragment.class);
+                    i.putExtra("idPersona", String.valueOf(view.getTag()));
+                    view.getContext().startActivity(i);
+                }else if(band==1){
+                    Toast.makeText(view.getContext(),
+                            "ID: " + String.valueOf(view.getTag()),
+                            Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(view.getContext(),VerificacionActivity.class);
+                    i.putExtra("idEquipo", String.valueOf(view.getTag()));
+                    view.getContext().startActivity(i);
+                }
             }
         });
     }
